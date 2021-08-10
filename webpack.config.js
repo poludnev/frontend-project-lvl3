24330bc36c4bfd6e1644c6e80331ac7b6eb36a3c
poludnev <poludnev.roman@gmail.com>
2021-08-10 22:24:11 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,6 @@ export default {
     filename: 'bundle.js',
     path: path.resolve(process.cwd(), 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      //     {
-      //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //       type: 'asset/resource',
-      //     },
-      //     {
-      //       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      //       type: 'asset/resource',
-      //     },
-    ],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
