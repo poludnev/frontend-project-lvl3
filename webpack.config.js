f447aa331d2252c3a63f3ddb7a2feed555e800e7
poludnev <poludnev.roman@gmail.com>
2021-08-11 00:03:57 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/template.html',
-      // template: './index.html',
+      template: './index.html',
     }),
   ],
 };
