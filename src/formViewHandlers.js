e23004a8c87535a2493dcb3c29368535f4b6e700
poludnev <poludnev.roman@gmail.com>
2021-08-15 01:38:17 +0300

    test

--- src/formViewHandlers.js
+++ src/formViewHandlers.js
@@ -1,4 +1,4 @@
-const invalidHandler = (state) => {
+const inValidHandler = (state) => {
   const input = document.querySelector('input');
   input.classList.add('is-invalid');
   input.removeAttribute('readonly');
@@ -31,7 +31,7 @@ const validHandler = (state) => {
   feedback.classList.remove('text-danger');
 };
 
-const requestingHandler = () => {
+const requestingHAndler = () => {
   const input = document.querySelector('input');
   input.classList.remove('is-invalid');
   input.disabled = true;
@@ -75,8 +75,10 @@ const makeFeedLi = (title, description) => {
 };
 
 const renderFeeds = (state) => {
+  // console.log('renderFeeds state', state);
   const feeds = document.querySelector('.feeds');
   feeds.innerHTML = '';
+  if (state.data.length === 0) return;
   feeds.appendChild(makeCard(state.title));
   const feedsUl = makeUl();
   const feedsData = state.data;
@@ -111,7 +113,14 @@ const makePostsLi = (title, description, link, id, visited, buttonsName) => {
   }
 
   a.innerHTML = `${title}`;
-
+  // a.addEventListener('click', (e) => {
+  //   e.target.classList.remove('fw-bold');
+  //   e.target.classList.add('fw-normal', 'link-secondary');
+  //   console.log('a target', e.target, e.target.dataset.id);
+  //   const post = _.find(watchedState.posts.data, { id: Number(e.target.dataset.id) });
+  //   post.visited = true;
+  //   console.log('pist find', post);
+  // });
   const button = document.createElement('button');
   button.type = 'button';
   button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
@@ -127,6 +136,7 @@ const makePostsLi = (title, description, link, id, visited, buttonsName) => {
 const renderPosts = (state) => {
   const posts = document.querySelector('.posts');
   posts.innerHTML = '';
+  if (state.data.length === 0) return;
   posts.appendChild(makeCard(state.title));
   const postsUl = makeUl();
   const postsData = state.data;
@@ -140,22 +150,22 @@ const renderPosts = (state) => {
 
 const viewHandlers = {
   requesting() {
-    requestingHandler();
+    requestingHAndler();
   },
   success(state) {
     validHandler(state);
   },
   urlExists(state) {
-    invalidHandler(state);
+    inValidHandler(state);
   },
   invalidURL(state) {
-    invalidHandler(state);
+    inValidHandler(state);
   },
   invalidRSS(state) {
-    invalidHandler(state);
+    inValidHandler(state);
   },
   networkError(state) {
-    invalidHandler(state);
+    inValidHandler(state);
   },
   feeds(state) {
     renderFeeds(state);
