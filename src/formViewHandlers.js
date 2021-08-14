0f427986727a37c79d915194aa8e271aa4c730fa
poludnev <poludnev.roman@gmail.com>
2021-08-15 01:39:56 +0300

    test

--- src/formViewHandlers.js
+++ src/formViewHandlers.js
@@ -61,7 +61,6 @@ const makeUl = () => {
 const makeFeedLi = (title, description) => {
   const li = document.createElement('li');
   li.classList.add('list-group-item', 'border-0', 'border-end-0');
-
   const h3 = document.createElement('h3');
   h3.classList.add('h6', 'm-0');
   h3.innerHTML = title;
@@ -69,8 +68,8 @@ const makeFeedLi = (title, description) => {
   const p = document.createElement('p');
   p.classList.add('m-0', 'small', 'text-black-50');
   p.innerHTML = description;
-
-  li.appendChild(h3, p);
+  li.appendChild(h3);
+  li.appendChild(p);
   return li;
 };
 
