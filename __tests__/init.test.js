48e2b0a0204648b58afc8d4398bcaf93394f516f
poludnev <poludnev.roman@gmail.com>
2021-08-29 21:14:21 +0300

    first check fixings

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,6 +1,8 @@
 import { expect, test } from '@jest/globals';
 import sum from '../src/sum.js';
 
+// тесты в процессе, пока вставил тест, только ради наличия и бейджей.
+// По ходу проверок буду добавлять.
 test('test1', () => {
   expect(sum(2, 3)).toBe(5);
 });
