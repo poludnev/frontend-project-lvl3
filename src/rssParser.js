109eb10f8e041c02829f27175958210a2b2fedd4
poludnev <poludnev.roman@gmail.com>
2021-09-11 10:41:11 +0300

    Check 2 RSS parser amendments
    Changed the items parsing structure from parsing by children nodes index to parsing by selectors

--- src/rssParser.js
+++ src/rssParser.js
@@ -1,5 +1,4 @@
 export default (data) => {
-  // console.log('parser started');
   const parser = new DOMParser();
   const document = parser.parseFromString(data, 'application/xml');
 
@@ -9,12 +8,10 @@ export default (data) => {
   const description = document.querySelector('description').textContent;
 
   const items = [...document.querySelectorAll('item')].map((item) => ({
-    title: item.children[0].textContent,
-    description: item.children[3].textContent,
-    link: item.children[2].textContent,
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    link: item.querySelector('link').textContent,
   }));
-
-  // console.log('parser accomplished');
 
   return { title, description, items };
 };
